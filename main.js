b51fd94872da07b1d085baa570d073a043de0ded
Vladimir Vertenz <vladmirworld@gmail.com>
2026-05-04 00:19:41 +0300

    feat(app): debug

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+const { platform } = require("node:process");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -6,9 +7,20 @@ const createWindow = () => {
     height: 600,
   });
 
+  console.log(platform);
+
   win.loadFile("index.html");
+  win.webContents.openDevTools();
 };
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (platform !== "darwin") app.quit();
 });
